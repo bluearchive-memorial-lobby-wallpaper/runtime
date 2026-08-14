78bdd46e764853e3d895d8fb548bb6ef67fd0102
FrozenlemonTee <1115306170@qq.com>
2026-08-14 18:47:05 +0800

    feat: expose runtime public API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./definition.ts";
+export * from "./validation.ts";
+export * from "./dialogue/DialoguePlaybackSequence.ts";
+export * from "./render/FrameLimiter.ts";
+export * from "./layout/viewport.ts";
+export * from "./layout/modelTransform.ts";
+export * from "./layout/subtitle.ts";
